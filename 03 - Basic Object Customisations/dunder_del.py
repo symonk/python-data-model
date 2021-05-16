13a45146a418638bb872b4f959b591e3a0d218ae
symonk <jackofspaces@gmail.com>
2021-05-16 12:22:59 +0100

    typo in `__del__()`

--- 03 - Basic Object Customisations/dunder_del.py
+++ 03 - Basic Object Customisations/dunder_del.py
@@ -13,8 +13,8 @@ their `__del__()` method twice, `CPython` currently will only attempt to call `_
 There is NO guarantee that `__del__()` on objects that still exist when the interpreter is exiting will be called,
 
 The builtin `del` is often confused with mapping directly to an objects `__del__()` invocation, for example it is
-often misunderstood that `del x` is not the equivalent of `x.__del__()`.  The former (`del x`) simple decrements
-the reference count of `x` by 1.  The latter `x.__del__()` is invoked when no references remain.
+often misunderstood that `del x` is not the equivalent of `x.__del__()`.  The former (`del x`) simply decrements
+the reference count of `x` by 1.  The latter `x.__del__()` is invoked when the reference count reaches 0.
 
 It is possible for the reference count of an object in python to be prevented from reaching 0 and subsequently firing
 `__del__()` on its instance.  As far as the reference count garbage collection goes this instance is in limbo,
